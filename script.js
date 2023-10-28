8cd4a124626505dcada348ca575999498eeb1ebd
Madhuri311 <139129777+Madhuri311@users.noreply.github.com>
2023-10-28 16:38:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10) {
+        $(".navbar").addClass("nav-scroll");
+    } else {
+        $(".navbar").removeClass("nav-scroll");
+    }
+    
+});
+
+$('.testimonial_slider').slick({
+    dots: true,
+    isfinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint:1024,
+            settings{
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }
+    ]
+})
---
+++ sign.js
@@ -0,0 +1,5 @@
+function validation(){
+if(document.Formfill.Username.value==""){
+    
+}
+}
